e06fc2fb121895f82c31f5412503fde641815984
antonkalmbah <anton.kalmbah@yandex.ru>
2023-04-24 22:17:54 +0300

    рекурсия

---
+++ recursion.js
@@ -0,0 +1,43 @@
+const rec = (value, fn) => {
+    fn(value);
+    return value > 0 ? rec(value - 1, fn) : value;
+};
+
+
+// -------------------------------------------------------------
+
+const recTime = (value, fn, delay = 1000) => {
+    fn(value);
+    return value > 0 
+        ? setTimeout(() => recTime(value - 1, fn), delay)
+        : value
+};
+
+// -------------------------------------------------------------
+
+// rec(100, value => console.log(value))
+// recTime(100, value => console.log(value))
+
+
+// -------------------------------------------------------------
+
+const dan = {
+    type: "person",
+    data: {
+        gender: "male",
+        info: {
+            id: 22,
+            fullname: {
+                first: "Dan",
+                last: "Deacon"
+            }
+        }
+    }
+};
+
+const danFunc = (fields, object = {}) => {
+    const[first, ...remaining] = fields.split(', ');
+    return remaining.lenght ? danFunc(remaining.join(', '), object[first]) : object[first];
+};
+
+danFunc("data.info.id", dan);
