7f15f7c14c396cd05148f51ddfca6fc534e46c31
antonkalmbah <anton.kalmbah@yandex.ru>
2023-05-16 21:20:41 +0300

    5 stars

--- recipes-app/src/components/IconsAction.js
+++ recipes-app/src/components/IconsAction.js
@@ -7,6 +7,6 @@ const Star = ({selected = false}) => (
 
 const createArray = length => [...Array(length)];
 
-export default function StarRating({ totalStars = 1 }) {
+export default function StarRating({ totalStars = 0 }) {
     return createArray(totalStars).map((n, i) => <Star key={i} />);
 }
