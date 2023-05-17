6028fa7130dcb5301a2eab9994e2fc144007dcfc
antonkalmbah <anton.kalmbah@yandex.ru>
2023-05-17 20:14:31 +0300

    first change for button delete

--- colors/src/components/App.js
+++ colors/src/components/App.js
@@ -3,6 +3,17 @@ import colorData from "../data/data.json";
 import ColorList from "./ColorList";
 
 export default function App() {
-    const [colors] = useState(colorData);
-    return <ColorList colors={colors} />;
+    // второй аргумент в массиве, возвращаемом useState, — это функция, которую мы можем использовать 
+    // для изменения состояния
+    const [colors, setColors] = useState(colorData);
+    return (
+        <ColorList 
+            colors={colors} 
+            onRemoveColor={id => {
+                const newColors = colors.filter(color => color.id !== id);
+                console.log(colors.id);
+                setColors(newColors);
+            }}
+        />
+    );
 }
--- colors/src/components/ColorList.js
+++ colors/src/components/ColorList.js
@@ -1,11 +1,11 @@
 import Color from "./Color";
 
-export default function ColorList({ colors = [] }) {
+export default function ColorList({ colors = [], onColorRemove = f => f }) {
     if(!colors.length) return <div>Нет цвет!</div>
     return (
         <div>
             {
-                colors.map(color => <Color key={color.id} {...color} />)
+                colors.map(color => <Color key={color.id} {...color} onRemove={onColorRemove} />)
             }
         </div>
     );
